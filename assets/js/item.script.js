3403775cee44bd90e5ed92af221c93433807134d
jttpbook <jittapatbook101@gmail.com>
2025-09-08 12:19:31 +0700

    update item again

--- assets/js/item.script.js
+++ assets/js/item.script.js
@@ -9,7 +9,7 @@ fetch("http://54.169.154.143:3096/food-menus", {
         result.forEach(food => {
             data += `
             <div class="card" style="width: 18rem; ">
-            <img src="${food.image}" class="card-img-top" style="height: 200px; object-fit: cover;" alt="xxx">
+            <img src="${food.image}" class="card-img-top mt-2" style="height: 200px; object-fit: cover;" alt="xxx">
             <div class="card-body">
               <h6 class="card-title text-danger">${food.price}</h6>
               <h5 class="card-title">${food.name} (${food.name_en})</h5>
