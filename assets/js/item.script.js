6b36aef8136b2c17e752716e851c68e87514de81
jttpbook <jittapatbook101@gmail.com>
2025-09-08 21:11:01 +0700

    อัพเดทล่าสุด

--- assets/js/item.script.js
+++ assets/js/item.script.js
@@ -19,7 +19,7 @@ function renderHotelRoom(foods) {
             data += `
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3">
                 <div class="card h-100 mx-auto" style="width: 17rem;">
-                    <img src="${f.image}" class="card-img-top object-fit-cover" style="height: 200px;" alt="picture">
+                    <img src="${f.image}" class="card-img-top object-fit-cover" style="height: 200px;" alt="${f.name}">
                     
                     <div class="card-body d-flex flex-column">
                     <p class="card-text">
@@ -30,7 +30,7 @@ function renderHotelRoom(foods) {
 
                     <!-- ปุ่มอยู่ล่างสุด -->
                     <div class="mt-auto text-center">
-                        <button type="button" class="btn btn-primary text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <button type="button" class="btn btn-primary text-white" data-bs-toggle="modal" data-bs-target="#modal-${f.id}">
                         รายละเอียด
                         </button>
                         <a href="#" class="btn btn-secondary text-white">สั่งอาหาร</a>
@@ -40,12 +40,12 @@ function renderHotelRoom(foods) {
                 </div>
 
                 <!-- Modal แยกออกมาจากการ์ด -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal fade" id="modal-${f.id}" tabindex="-1" aria-labelledby="modalLabel-${f.id}" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content px-3">
                         <div class="modal-header-row">
-                            <h1 class="modal-title fs-5 my-3" id="exampleModalLabel">รายละเอียด</h1>
-                            <img src="${f.image}" class="card-img-top mt-2" style="height: 200px; object-fit: cover;" alt="xxx">
+                            <h1 class="modal-title fs-5 my-3" id="modalLabel-${f.id}">รายละเอียด</h1>
+                            <img src="${f.image}" class="card-img-top mt-2 rounded-3" style="height: 300px; object-fit: cover;" alt="${f.name}">
                             <h6 class="card-title text-danger pt-2  pb-2">${"ราคา"} ${f.price} ${"บาท"}</h6>
                             <h5 class="card-title text-black">${f.name} (${f.name_en})</h5>
                             <p class="card-text">${f.description}</p>
@@ -56,7 +56,7 @@ function renderHotelRoom(foods) {
                             <p class="card-text">${f.ingredients}</p>
                             <div class="alert alert-danger d-flex align-items-center" role="alert">
                             <i class="bi bi-exclamation-triangle-fill me-2"></i>
-                            <span>⚠ อาหารนี้มีส่วนผสมของ: <strong>${f.allergens}</strong></span>
+                            <span>⚠ ห้ามผู้ที่มีอาการแพ้ <strong>"${f.allergens}"</strong> รับประทานโดยเด็ดขาด</span>
                             </div>
                         </div>
                         <div class="modal-footer">
